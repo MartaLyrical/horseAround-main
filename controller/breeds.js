5359dae09b024e26fa2f5fac30ea423a6e73d265
MartaLyrical <mhsdomingos@gmail.com>
2023-03-18 23:41:15 +0100

    breeds endpoint

--- controller/breeds.js
+++ controller/breeds.js
@@ -1,13 +1,106 @@
-const mongodb = require('../db/mongoClientDb')
+const mongodb = require("../db/mongoClientDb");
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('breeds').find()
+  const result = await mongodb.getDb().db().collection("breeds").find();
+  result.toArray().then((lists) => {
+    res.status(200).json(lists);
+  });
+};
+const getOne = async (req, res) => {
+  try {
+    const breedId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("breeds")
+      .find({ _id: breedId });
     result.toArray().then((lists) => {
-        res.status(200).json(lists);
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists[0]);
     });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
+const createBreed = async (req, res) => {
+  console.log("Adding a new horse breed...");
+  const breed = {
+    breedName: req.body.breedName,
+    color: req.body.color,
+    height: req.body.height,
+    weight: req.body.weight,
+    type: req.body.type,
+    origin: req.body.origin,
+    features: req.body.features,
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("breeds")
+    .insertOne(breed);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || "Error, horse breed not created");
+  }
+};
+
+const updateBreed = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json("Must use a horse breed id to update a breed.");
+  }
+  const breedId = new ObjectId(req.params.id);
+  const breed = {
+    breedName: req.body.breedName,
+    color: req.body.color,
+    height: req.body.height,
+    weight: req.body.weight,
+    type: req.body.type,
+    origin: req.body.origin,
+    features: req.body.features,
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("breeds")
+    .replaceOne({ _id: breedId }, breed);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Error, the horse breed was not updated!!!");
+  }
+};
+
+const deleteBreed = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json("Must use a valid horse breed id to delete a breed.");
+  }
+  const breedId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("breeds")
+    .deleteOne({ _id: breedId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while deleting the horse breed."
+      );
+  }
+};
 
 module.exports = {
-    getAll
+  getAll,
+  getOne,
+  createBreed,
+  updateBreed,
+  deleteBreed,
 };
--- routes/breeds.js
+++ routes/breeds.js
@@ -1,12 +1,14 @@
-const express = require('express')
+const express = require("express");
 
-const breedsRouter = express.Router()
-const breedsController = require('../controller/breeds')
-const { tryCatch } = require("../utils/tryCatch")
+const breedsRouter = express.Router();
+const breedsController = require("../controller/breeds");
+const { tryCatch } = require("../utils/tryCatch");
 
+breedsRouter.get("/", tryCatch(breedsController.getAll));
 
-breedsRouter.get('/', tryCatch(
-    breedsController.getAll
-))
+breedsRouter.get("/:id", breedsController.getOne);
+breedsRouter.post("/", breedsController.createBreed);
+breedsRouter.put("/:id", breedsController.updateBreed);
+breedsRouter.delete("/:id", breedsController.deleteBreed);
 
-module.exports = breedsRouter
+module.exports = breedsRouter;
