f06ce71e30993a84fc01215f58951baf9ca8be14
jeremiahMiclat <mic19017@byui.edu>
2023-03-26 06:08:10 +0800

    Revert "errors and status codes added"
    This reverts commit 9445e84b281c9143cc4bf5fc3a1f715f1eedf348.

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,31 +1,6 @@
 const errorHandler = (error, req, res, next) => {
-    let statusCode = 500
-    switch (error.name) {
-        case "CastError":
-            statusCode = 400 // Bad Request
-            break;
-        case "ValidationError":
-            statusCode = 422 // Unprocessable Entity
-            break;
-        case "MongoError":
-            statusCode = 503 // Service Unavailable
-            break;
-        case "DuplicateKeyError":
-            statusCode = 409 // Conflict
-            break;
-        case "QueryError":
-            statusCode = 400 // Bad Request
-            break;
-        case "BulkWriteError":
-            statusCode = 500 // Internal Server Error
-            break;
-        // Add more cases for different types of errors you might think of below
-        default:
-            statusCode = 500 // Internal Server Error
-            break;
-    }
-    return res.status(statusCode).send(error.message)
-}
-
+    console.log(error.name)
+    return res.status(500).send('Something went wrong')
+};
 
 module.exports = errorHandler
