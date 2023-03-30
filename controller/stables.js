4dd638cdd53316aed40e71b806b76d2520edb694
jeremiahMiclat <mic19017@byui.edu>
2023-03-31 04:15:24 +0800

    stables update

--- controller/stables.js
+++ controller/stables.js
@@ -1,59 +1,140 @@
-const stablesSchema = require("../Schema/stables");
-const stablesOrderSchema = require("../Schema/order");
+const stablesSchema = require("../Schema/stables")
+const stablesOrderSchema = require("../Schema/order")
+const { IDNotFound, DuplicateError } = require('../errors/customErrors')
 
 // GET/stables/inventory
-const getInventory = async (req, res) => {
-  const stablesInventory = await stablesSchema.find();
-  const numberOfHorses = stablesInventory.reduce((total, stablesSchema) => total + stablesSchema.numberOfHorses, 0)
+const getInventory = async(req, res) => {
+    const stablesInventory = await stablesSchema.find()
+    const numberOfHorses = stablesInventory.reduce((total, stablesSchema) => total + stablesSchema.numberOfHorses, 0)
 
-  res.status(200).json(numberOfHorses);
-};
+    res.status(200).json(numberOfHorses)
+}
 
 // GET/allstables/
-const getAll = async (req, res) => {
-  const allStables = await stablesSchema.find();
-  res.status(200).json(allStables);
-};
+const getAll = async(req, res) => {
+    const allStables = await stablesSchema.find()
+    res.status(200).json(allStables)
+}
 
 // GET/stables/{stablesId}
-const getOne = async (req, res) => {
-  const stable = await stablesSchema.findById(req.params.id)
+const getOne = async(req, res) => {
+    const { id } = req.params
+    const stable = await stablesSchema.findById(id)
 
-  if (!stable) {
-    throw new Error('ID not found')
-  }
+    if (!stable) {
+        const error = new IDNotFound(`Stable with ID ${id} not found`)
+        throw error
+    }
 
-  res.status(200).json(stable)
+    res.status(200).json(stable)
 }
 
 // POST/stables/order
-const orderOne = async (req, res) => {
+const orderOne = async(req, res) => {
 
-  const orderReq = new stablesOrderSchema({
-    horseId: req.body.horseId,
-    stablesId: req.body.horseId
-  })
+    const orderReq = new stablesOrderSchema({
+        horseId: req.body.horseId,
+        stablesId: req.body.horseId
+    })
 
-  console.log(orderReq.horseId)
+    const existingOrder = await stablesOrderSchema.findOne({ horseId: orderReq.horseId })
 
-  const existingOrder = await stablesOrderSchema.findOne({ horseId: orderReq.horseId })
-
-  if (existingOrder) {
-    console.log("Order exist")
-    return res.status(400).json({ message: `Horse with ID ${orderReq.horseId} already exists` });
-  }
-  const newOrder = await orderReq.save()
-  res.status(201).json(newOrder._id)
+    if (existingOrder) {
+        const error = new DuplicateError(`Order horse with ID ${orderReq.horseId} is not available`)
+        throw error
+    }
+    const newOrder = await orderReq.save()
+    res.status(201).json(newOrder._id)
 
 
+}
+
+// POST/stables/
+const createOne = async(req, res) => {
+
+    const stable = new stablesSchema({
+        name: req.body.name,
+        location: req.body.location,
+        owner: req.body.owner,
+        numberOfHorses: req.body.numberOfHorses
+    })
+
+    const createdStable = await stablesSchema.findOne({
+        $and: [
+            { name: stable.name },
+            { location: stable.location },
+            { owner: stable.owner }
+        ]
+    })
+
+    if (createdStable) {
+        const error = new DuplicateError(`Stable ${stable.name} already on DB`)
+        throw error
+    }
+    const newStable = await stable.save()
+    res.status(201).json(newStable._id)
+
+}
+
+// PUT/stables/{stablesId}
+const updateOne = async(req, res) => {
+    const { id } = req.params
+    const { name, location, owner, numberOfHorses } = req.body
+
+    // Check if stable with given ID exists
+    const existingStableById = await stablesSchema.findById(id)
+    if (!existingStableById) {
+        const error = new IDNotFound(`Stable with ID ${id} not found`)
+        throw error
+    }
+
+    // Check if stable with same name, location, and owner already exists
+    const existingStable = await stablesSchema.findOne({
+        $and: [
+            { name },
+            { location },
+            { owner },
+            { numberOfHorses }
+        ]
+    })
+
+    if (existingStable && existingStable.id.toString() !== id) {
+        const error = new DuplicateError(`This update creates a duplicate of another saved stable data`)
+        throw error
+    }
+
+    if (existingStable && existingStable._id.toString() == id) {
+        const error = new DuplicateError(`The data you entered has the same data to the original one`)
+        throw error
+
+    }
+
+    // Update stable
+    const updatedStable = await stablesSchema.findByIdAndUpdate(
+        id, {
+            name,
+            location,
+            owner,
+            numberOfHorses
+        }, { new: true, runValidators: true }
+    )
+
+    res.status(200).json(updatedStable)
 }
 
 // DELETE/stables/{stablesId}
-const deleteOne = async (req, res) => {
+const deleteOne = async(req, res) => {
+    const { id } = req.params
+    const stable = await stablesSchema.findById(id)
 
-  const removedStables = await stablesSchema.deleteOne({ _id: req.params.id })
-  res.status(200).json(removedStables)
+    if (!stable) {
+        const error = new IDNotFound(`Stable with ID ${id} not found`)
+        throw error
+    }
+
+    const removedStables = await stablesSchema.deleteOne({ _id: req.params.id })
+    res.status(200).json(removedStables)
 
 }
 
-module.exports = { getInventory, getAll, getOne, orderOne, deleteOne };
+module.exports = { getInventory, getAll, getOne, createOne, orderOne, updateOne, deleteOne }
--- routes/stables.js
+++ routes/stables.js
@@ -4,46 +4,60 @@ const stablesRouter = express.Router()
 const stablesController = require('../controller/stables')
 const { tryCatch } = require("../utils/tryCatch")
 
-// GET/allstables/
+// GET /stables/
 stablesRouter.get('/',
-    tryCatch(
-        // #swagger.tags = ['Stables']
-        // #swagger.description = 'Displays an array of Stables document.'
-        /* #swagger.responses[200] = { 
-                  description: 'Returns all stables document in stables collection',
-                  schema: { $ref: "#/definitions/Stables" }
-             } */
-        stablesController.getAll
+        tryCatch(
+            // #swagger.tags = ['Stables']
+            // #swagger.description = 'Displays an array of Stables document.'
+            /* #swagger.responses[200] = { 
+                      description: 'Returns all stables document in stables collection',
+                      schema: { $ref: "#/definitions/Stables" }
+                 } */
+            stablesController.getAll
+        )
     )
-)
-
-// GET/stables/inventory
+    // GET /stables/inventory
 stablesRouter.get('/inventory',
     tryCatch(
         stablesController.getInventory
     )
 )
 
-// POST/stables/order
-stablesRouter.post('/order',
-    tryCatch(
-        stablesController.orderOne
-    )
-)
-
-// GET/stables/{stablesId}
+// GET /stables/{stablesId}
 stablesRouter.get('/:id',
     tryCatch(
         stablesController.getOne
     )
 )
 
+// POST /stables/
+stablesRouter.post('/',
+    tryCatch(
+        stablesController.createOne
+    )
+)
 
-// DELETE/stables/{stablesId}
+// PUT /stables/{stablesId}
+stablesRouter.put('/:id',
+    tryCatch(
+        stablesController.updateOne
+    )
+)
+
+// DELETE /stables/{stablesId}
 stablesRouter.delete('/:id',
     tryCatch(
         //swagger docs code goes here
         stablesController.deleteOne
+    )
+)
+
+
+
+// POST /stables/order
+stablesRouter.post('/order',
+    tryCatch(
+        stablesController.orderOne
     )
 )
 
