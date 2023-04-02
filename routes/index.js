88107063d9b925bfc7ce0a344e8ad54b37a2884a
xphile1998 <eron.whitefield@gmail.com>
2023-04-01 17:18:56 -0700

    Adding in OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
-const express = require('express')
+const express = require('express');
+const { auth } = require('express-openid-connect');
 
 const router = express.Router()
 // router.use("/", require("./swagger"));
 
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// For OAuth authentication
+router.use(auth(config));
+
 // for testing only
+// router.get('/', (req, res) => {
+//     res.send('Home')
+// })
 router.get('/', (req, res) => {
-    res.send('Home')
-})
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 // routes goes here
 router.use('/stables', require('./stables'))
